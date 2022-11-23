c004d6faf72c83aa8198349e0b1666142ed70f8e
zichun-xu <zichun.xu@yale.edu>
2022-11-23 17:14:40 -0500

    add dependency

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -18,6 +18,7 @@ test_that(
 test_that(
   "The accel_plot() is correct for time-series data.",
   {
+    library(vdiffr)
     data(ukb_accel)
     p <- accel_plot(ukb_accel[1:100, ])
     expect_doppelganger("first-100-samples", p)
