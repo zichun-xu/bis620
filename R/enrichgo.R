731ff7c7aedb2ee49bc41e6dc33c7920bde8bede
zichun-xu <zichun.xu@yale.edu>
2022-12-22 00:36:07 -0500

    namespace

--- R/enrichgo.R
+++ R/enrichgo.R
@@ -4,7 +4,6 @@
 #' @param universe background genes to correct for
 #' @return a list of GO enrichment results for each gene cluster
 #' @importFrom  clusterProfiler enrichGO
-#' @import  enrichplot
 #' @import org.Hs.eg.db
 #' @export
 EnrichGO = function(clusters, universe = NULL){
